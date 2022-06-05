ecc9889c8e3791dccad82ac84001443a41674fd3
DELTA31TECH <d31tcjg@gmail.com>
2022-06-05 10:14:36 -0500

    Added PeopleController & View & Error Handling
    Learned about bundles and controllers. Where to place your custom JavaScript.
    Learned about routing. How to load data on to web pages and how to perform error handling.

---
+++ MVC-Demo/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Demo.Models;
+
+namespace MVC_Demo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            // Intentional Error to show how Errors are handled.
+            PersonModel p = new PersonModel();
+            int i = 5 / p.Age;
+
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel{ FirstName = "Cesar", LastName = "Garcia", Age = 28});
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Corey", Age = 38 });
+            people.Add(new PersonModel { FirstName = "Sarah", LastName = "Connor", Age = 25 });
+
+            return View(people);
+        }
+    }
+}
